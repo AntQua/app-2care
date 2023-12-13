e058eb7ecfe901002ff6912cd83009cfa39c1a32
Antonio Quaresma <tozequa@gmail.com>
2023-12-13 11:37:11 +0000

    update app domain in smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :user_name =>"apikey", # This is the string literal ‘apikey’, NOT the ID of your API key
   :password => ENV["SENDGRID_APIKEY"], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => "www.2care.me",
+  :domain => "app-2care-7e485396212c.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
